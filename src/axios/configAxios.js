83b32d54d69d1f991d1d22ee2161429d26e0e56a
Jerome <ducampdorgasjerome@gmail.com>
2021-11-10 13:20:16 +0100

    headers authorization

--- src/axios/configAxios.js
+++ src/axios/configAxios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://lsf-back.herokuapp.com/api/ ",
 });
-/*instance.defaults.headers.common["Authorization"] = localStorage.getItem(
+instance.defaults.headers.common["Authorization"] = localStorage.getItem(
   "token"
-);*/
+);
 
 export default instance;
